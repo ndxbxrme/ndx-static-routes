692d6a4a5a85964417748c1d8fa8baa7b1c0149b
ndxbxrme <lewis_the_cat@hotmail.com>
2017-01-20 22:19:30 +0000

    express.static

--- build/index.js
+++ build/index.js
@@ -1,18 +1,18 @@
 (function() {
   'use strict';
-  var gzippo;
+  var express;
 
-  gzippo = require('gzippo');
+  express = require('express');
 
   module.exports = function(ndx) {
-    ndx.app.use('/scripts', gzippo.staticGzip('./build/client/scripts'));
-    ndx.app.use('/images', gzippo.staticGzip('./build/client/images'));
-    ndx.app.use('/styles', gzippo.staticGzip('./build/client/styles'));
-    ndx.app.use('/views', gzippo.staticGzip('./build/client/views'));
-    ndx.app.use('/public', gzippo.staticGzip('./public'));
-    ndx.app.use('/favicon', gzippo.staticGzip('./favicon'));
-    ndx.app.use('/bower', gzippo.staticGzip('./bower'));
-    ndx.app.use('/build/client', gzippo.staticGzip('./build/client'));
+    ndx.app.use('/scripts', express["static"]('./build/client/scripts'));
+    ndx.app.use('/images', express["static"]('./build/client/images'));
+    ndx.app.use('/styles', express["static"]('./build/client/styles'));
+    ndx.app.use('/views', express["static"]('./build/client/views'));
+    ndx.app.use('/public', express["static"]('./public'));
+    ndx.app.use('/favicon', express["static"]('./favicon'));
+    ndx.app.use('/bower', express["static"]('./bower'));
+    ndx.app.use('/build/client', express["static"]('./build/client'));
     return ndx.app.all('/*', function(req, res) {
       return res.sendFile('index.html', {
         root: './build/client'
--- src/index.coffee
+++ src/index.coffee
@@ -1,17 +1,17 @@
 'use strict'
 
-gzippo = require 'gzippo'
+express = require 'express'
 
 
 
 module.exports = (ndx) ->
-  ndx.app.use '/scripts', gzippo.staticGzip './build/client/scripts'
-  ndx.app.use '/images', gzippo.staticGzip './build/client/images'
-  ndx.app.use '/styles', gzippo.staticGzip './build/client/styles'
-  ndx.app.use '/views', gzippo.staticGzip './build/client/views'
-  ndx.app.use '/public', gzippo.staticGzip './public'
-  ndx.app.use '/favicon', gzippo.staticGzip './favicon'
-  ndx.app.use '/bower', gzippo.staticGzip './bower'
-  ndx.app.use '/build/client', gzippo.staticGzip './build/client'
+  ndx.app.use '/scripts', express.static('./build/client/scripts')
+  ndx.app.use '/images', express.static('./build/client/images')
+  ndx.app.use '/styles', express.static('./build/client/styles')
+  ndx.app.use '/views', express.static('./build/client/views')
+  ndx.app.use '/public', express.static('./public')
+  ndx.app.use '/favicon', express.static('./favicon')
+  ndx.app.use '/bower', express.static('./bower')
+  ndx.app.use '/build/client', express.static('./build/client')
   ndx.app.all '/*', (req, res) ->
     res.sendFile 'index.html', root: './build/client'
