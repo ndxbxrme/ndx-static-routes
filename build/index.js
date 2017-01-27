414d09ac85f2446d3923ce2de168ceec648527e8
ndxbxrme <lewis_the_cat@hotmail.com>
2017-01-27 18:10:47 +0000

    modified live routes

--- build/index.js
+++ build/index.js
@@ -1,30 +1,31 @@
 (function() {
   'use strict';
   module.exports = function(ndx) {
-    if ((process.env.NODE_ENV || ndx.settings.NODE_ENV) === 'live') {
-      ndx.app.use('/app', ndx["static"]('./dist/app'));
-      ndx.app.use('/public', ndx["static"]('./public'));
-      ndx.app.use('/favicon', ndx["static"]('./favicon'));
-      return ndx.app.all('/*', function(req, res) {
-        return res.sendFile('index.html', {
-          root: './dist'
+    return process.nextTick(function() {
+      if ((process.env.NODE_ENV || ndx.settings.NODE_ENV) === 'live') {
+        ndx.app.use('/app', ndx["static"]('./dist/app'));
+        ndx.app.use('/public', ndx["static"]('./public'));
+        return ndx.app.all('/*', function(req, res) {
+          return res.sendFile('index.html', {
+            root: './dist'
+          });
         });
-      });
-    } else {
-      ndx.app.use('/scripts', ndx["static"]('./build/client/scripts'));
-      ndx.app.use('/images', ndx["static"]('./build/client/images'));
-      ndx.app.use('/styles', ndx["static"]('./build/client/styles'));
-      ndx.app.use('/views', ndx["static"]('./build/client/views'));
-      ndx.app.use('/public', ndx["static"]('./public'));
-      ndx.app.use('/favicon', ndx["static"]('./favicon'));
-      ndx.app.use('/bower', ndx["static"]('./bower'));
-      ndx.app.use('/build/client', ndx["static"]('./build/client'));
-      return ndx.app.all('/*', function(req, res) {
-        return res.sendFile('index.html', {
-          root: './build/client'
+      } else {
+        ndx.app.use('/scripts', ndx["static"]('./build/client/scripts'));
+        ndx.app.use('/images', ndx["static"]('./build/client/images'));
+        ndx.app.use('/styles', ndx["static"]('./build/client/styles'));
+        ndx.app.use('/views', ndx["static"]('./build/client/views'));
+        ndx.app.use('/public', ndx["static"]('./public'));
+        ndx.app.use('/favicon', ndx["static"]('./favicon'));
+        ndx.app.use('/bower', ndx["static"]('./bower'));
+        ndx.app.use('/build/client', ndx["static"]('./build/client'));
+        return ndx.app.all('/*', function(req, res) {
+          return res.sendFile('index.html', {
+            root: './build/client'
+          });
         });
-      });
-    }
+      }
+    });
   };
 
 }).call(this);
